869ab9861a2128bbaf34e807ee500e08ff71329d
Alexey Voitenko <alexeyvoitenko@MacBook-Pro-Alexey.local>
2015-04-08 16:51:44 +0300

    Initial Commit

---
+++ AzovNews/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AzovNews
+//
+//  Created by Alexey Voitenko on 08.04.15.
+//  Copyright (c) 2015 programmersfamily. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
